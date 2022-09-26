f1e8393f08f779e0cbca45df28be501dcba0a14e
Astrid Geels <ageels@f0r4s12.codam.nl>
2022-09-26 20:50:31 +0200

    signals

--- src/prompt.c
+++ src/prompt.c
@@ -6,28 +6,17 @@
 /*   By: ageels <ageels@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/09/26 18:05:32 by ageels        #+#    #+#                 */
-/*   Updated: 2022/09/26 20:32:51 by ageels        ########   odam.nl         */
+/*   Updated: 2022/09/26 20:45:05 by ageels        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
-
-void	my_signals(void)
-{
-	int			my_signal;
-	sigset_t	my_first_set;
-
-	my_signal = 0;
-	sigemptyset(&my_first_set);
-	sigaddset(&my_first_set, my_signal);
-}
 
 int	prompt(void)
 {
 	char		*line;
 
 	line = NULL;
-	my_signals();
 	while (1)
 	{
 		if (line)
