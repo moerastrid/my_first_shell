1509bfce9eda0e8de971be1cddadfb6eb9c2767f
Astrid Geels <ageels@f0r4s22.codam.nl>
2022-10-10 22:07:38 +0200

    leaks

--- src/execute/buildin.c
+++ src/execute/buildin.c
@@ -6,7 +6,7 @@
 /*   By: ageels <ageels@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/10 20:45:09 by ageels        #+#    #+#                 */
-/*   Updated: 2022/10/10 21:40:05 by ageels        ########   odam.nl         */
+/*   Updated: 2022/10/10 22:06:55 by ageels        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 int	is_del(char c)
 {
+	if (!c)
+		return (0);
 	if (c == ' ' || c == '\0' || c == '\n')
 		return (1);
 	return (0);
@@ -24,37 +26,37 @@ int	is_buildin(t_simple	*t)
 	if (!t)
 		return (0);
 	printf("argc: %d\nargv: %s\nbin: %s\n\n", t->argc, t->argv[0], t->bin);
-	if (ft_strncmp(t->argv[0], "echo", 4) == 0 && is_del(t->argv[0][5]) == 1)
+	if (ft_strncmp(t->argv[0], "echo", 5) == 0)
 		return (1);
-	if (ft_strncmp(t->argv[0], "cd", 2) == 0 && is_del(t->argv[0][3]) == 1)
+	if (ft_strncmp(t->argv[0], "cd", 3) == 0)
 		return (1);
-	if (ft_strncmp(t->argv[0], "pwd", 3) == 0 && is_del(t->argv[0][4]) == 1)
+	if (ft_strncmp(t->argv[0], "pwd", 4) == 0)
 		return (1);
-	if (ft_strncmp(t->argv[0], "export", 6) == 0 && is_del(t->argv[0][7]) == 1)
+	if (ft_strncmp(t->argv[0], "export", 7) == 0)
 		return (1);
-	if (ft_strncmp(t->argv[0], "unset", 5) == 0 && is_del(t->argv[0][6]) == 1)
+	if (ft_strncmp(t->argv[0], "unset", 6) == 0)
 		return (1);
-	if (ft_strncmp(t->argv[0], "env", 3) == 0 && is_del(t->argv[0][4]) == 1)
+	if (ft_strncmp(t->argv[0], "env", 4) == 0)
 		return (1);
-	if (ft_strncmp(t->argv[0], "exit", 4) == 0 && is_del(t->argv[0][5]) == 1)
+	if (ft_strncmp(t->argv[0], "exit", 5) == 0)
 		return (1);
 	return (0);
 }
 
 void	exec_buildin(t_cmd cmd, t_token *tokens)
 {
-	if (ft_strncmp(cmd.simples->argv[0], "echo", 4) == 0 && is_del(cmd.simples->argv[0][5]) == 1)
+	if (ft_strncmp(cmd.simples->argv[0], "echo", 5) == 0)
 		bi_echo();
-	if (ft_strncmp(cmd.simples->argv[0], "cd", 2) == 0 && is_del(cmd.simples->argv[0][3]) == 1)
+	if (ft_strncmp(cmd.simples->argv[0], "cd", 3) == 0)
 		bi_cd();
-	if (ft_strncmp(cmd.simples->argv[0], "pwd", 3) == 0 && is_del(cmd.simples->argv[0][4]) == 1)
+	if (ft_strncmp(cmd.simples->argv[0], "pwd", 4) == 0)
 		bi_pwd();
-	if (ft_strncmp(cmd.simples->argv[0], "export", 6) == 0 && is_del(cmd.simples->argv[0][7]) == 1)
+	if (ft_strncmp(cmd.simples->argv[0], "export", 7) == 0)
 		bi_export();
-	if (ft_strncmp(cmd.simples->argv[0], "unset", 5) == 0 && is_del(cmd.simples->argv[0][6]) == 1)
+	if (ft_strncmp(cmd.simples->argv[0], "unset", 6) == 0)
 		bi_unset();
-	if (ft_strncmp(cmd.simples->argv[0], "env", 3) == 0 && is_del(cmd.simples->argv[0][4]) == 1)
+	if (ft_strncmp(cmd.simples->argv[0], "env", 4) == 0)
 		bi_env();
-	if (ft_strncmp(cmd.simples->argv[0], "exit", 4) == 0 && is_del(cmd.simples->argv[0][5]) == 1)
+	if (ft_strncmp(cmd.simples->argv[0], "exit", 5) == 0)
 		bi_exit(cmd, tokens);
 }
