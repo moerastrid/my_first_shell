760832ae462d49fdf560d10756d6e61b5ed09c99
Astrid Geels <moerastrid@gmail.com>
2022-10-14 18:57:10 +0200

    pwd

--- src/execute/builtins/pwd.c
+++ src/execute/builtins/pwd.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   pwd.c                                              :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: ageels <ageels@student.codam.nl>           +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/09/12 14:12:13 by ageels            #+#    #+#             */
-/*   Updated: 2022/10/14 18:40:49 by tnuyten          ###   ########.fr       */
+/*                                                        ::::::::            */
+/*   pwd.c                                              :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: ageels <ageels@student.codam.nl>             +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/09/12 14:12:13 by ageels        #+#    #+#                 */
+/*   Updated: 2022/10/14 18:53:29 by ageels        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,7 @@
 
 int	bi_pwd(t_cmd cmd)
 {
-	char	buf[10000];
-
-	ft_putstr_fd("buildin pwd is executed\n", STDERR_FILENO);
-	ft_putstr_fd(getcwd(NULL, 10000), STDERR_FILENO);
+	ft_putstr_fd(getcwd(NULL, 0), STDERR_FILENO);
 	ft_putstr_fd("\n", STDERR_FILENO);
 	return (0);
 }
