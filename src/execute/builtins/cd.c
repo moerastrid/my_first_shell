5e034c4a50815f0b89e2f1925018188130274669
Astrid Geels <moerastrid@gmail.com>
2022-10-17 16:18:06 +0200

    jfasfj

--- src/execute/builtins/cd.c
+++ src/execute/builtins/cd.c
@@ -6,16 +6,17 @@
 /*   By: ageels <ageels@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/09/12 14:12:13 by ageels        #+#    #+#                 */
-/*   Updated: 2022/10/17 15:38:27 by ageels        ########   odam.nl         */
+/*   Updated: 2022/10/17 16:16:38 by ageels        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../execute.h"
 
-int	bi_cd(t_simple *simple)
+int	bi_cd(t_simple *simple, t_cmd *cmd)
 {
 	if (simple->argc == 1)
-		chdir("~");
+		chdir(find_str("HOME", cmd->envc) + 5);
+		// go to home dir function
 	else
 		chdir(simple->argv[1]);
 	// update pwd in envc
