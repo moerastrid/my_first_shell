f486be262735f67d89c565ccea4f3b77c69b6a25
Astrid Geels <moerastrid@gmail.com>
2022-10-21 23:42:29 +0200

    exit code

--- src/execute/builtins/exit.c
+++ src/execute/builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: ageels <ageels@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/09/12 14:12:13 by ageels        #+#    #+#                 */
-/*   Updated: 2022/10/20 20:55:39 by ageels        ########   odam.nl         */
+/*   Updated: 2022/10/21 23:40:53 by ageels        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,13 @@
 
 void	bi_exit(t_cmd *cmd)
 {
+	int ret;
+
+	ret = 0;
 	if (cmd->cmd_count == 1)
 		ft_putstr_fd("exit\n", STDERR_FILENO);
+	if (cmd->simples->argv[1])
+		ret = ft_atoi(cmd->simples->argv[1]);
 	clear_cmd(cmd);
-	exit (0);
+	exit (ret);
 }
--- src/execute/builtins/export.c
+++ src/execute/builtins/export.c
@@ -6,7 +6,7 @@
 /*   By: ageels <ageels@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/09/12 14:12:13 by ageels        #+#    #+#                 */
-/*   Updated: 2022/10/17 14:28:30 by ageels        ########   odam.nl         */
+/*   Updated: 2022/10/21 23:23:16 by ageels        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,17 @@ int	bi_export(t_simple simple, t_cmd *cmd)
 {
 	int	i;
 
+	i = 0;
+	if (!simple.argv[1])
+	{
+		while (cmd->envc[i] != NULL)
+		{
+			ft_putstr_fd("declare -x ", STDOUT_FILENO);
+			ft_putstr_fd(cmd->envc[i], STDOUT_FILENO);
+			ft_putstr_fd("\n", STDOUT_FILENO);
+			i++;
+		}
+	}
 	i = 1;
 	while (simple.argv[i])
 	{
