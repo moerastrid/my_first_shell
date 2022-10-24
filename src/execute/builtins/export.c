eefeaae8478bfb6f003736f8c3d564cd10830555
Astrid Geels <moerastrid@gmail.com>
2022-10-24 23:08:34 +0200

    export without comments but with quotes after =

--- src/execute/builtins/export.c
+++ src/execute/builtins/export.c
@@ -6,7 +6,7 @@
 /*   By: ageels <ageels@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/09/12 14:12:13 by ageels        #+#    #+#                 */
-/*   Updated: 2022/10/24 22:43:35 by ageels        ########   odam.nl         */
+/*   Updated: 2022/10/24 23:07:44 by ageels        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,20 +18,20 @@ int	bi_export(t_simple simple, t_cmd *cmd)
 	int		j;
 
 	i = 0;
-	j = 0;
 	if (!simple.argv[1])
 	{
 		while (cmd->envc[i] != NULL)
 		{
 			ft_putstr_fd("declare -x ", STDOUT_FILENO);
-			//while (cmd->envc[i][j] != '\0')
-			//{
-			//	ft_putchar_fd(cmd->envc[i][j], STDOUT_FILENO);
-			//	if (cmd->envc[i][j] == '=')
-			//		ft_putstr_fd("\"", STDOUT_FILENO);
-			//	j++;
-			//}
-			//ft_putstr_fd("\"\n", STDOUT_FILENO);
+			j = 0;
+			while (cmd->envc[i][j] != '\0')
+			{
+				ft_putchar_fd(cmd->envc[i][j], STDOUT_FILENO);
+				if (cmd->envc[i][j] == '=')
+					ft_putchar_fd('\"', STDOUT_FILENO);
+				j++;
+			}
+			ft_putstr_fd("\"\n", STDOUT_FILENO);
 			i++;
 		}
 	}
