c1bf4d94acf50aa866b116fcd351f3324383e116
Astrid Geels <moerastrid@gmail.com>
2022-10-24 23:26:51 +0200

    exit command gives an error message if not exited with a numeric argument

--- src/execute/builtins/exit.c
+++ src/execute/builtins/exit.c
@@ -6,24 +6,65 @@
 /*   By: ageels <ageels@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/09/12 14:12:13 by ageels        #+#    #+#                 */
-/*   Updated: 2022/10/21 23:40:53 by ageels        ########   odam.nl         */
+/*   Updated: 2022/10/24 23:25:22 by ageels        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../execute.h"
 
+int	ft_atoi_ret(const char *str, int *ret)
+{
+	int	a;
+	int	result;
+	int	mincounter;
+
+	a = 0;
+	result = 0;
+	mincounter = 1;
+	if ((str[a] == '+') || (str[a] == '-'))
+	{
+		if (str[a] == '-')
+			mincounter = mincounter * -1;
+		a++;
+	}
+	while (str[a] != '\0')
+	{
+		if (ft_isdigit(str[a]) != 1)
+		{
+			*ret = -1;
+			g_errno = 255;
+			return (0);
+		}
+		result = (result * 10);
+		result = (result + (str[a] - '0'));
+		a++;
+	}
+	return (result * mincounter);
+}
+
 void	bi_exit(t_cmd *cmd, t_simple *simple)
 {
 	int	ret;
+	int	num;
 
+	num = 0;
 	ret = 0;
-	if (cmd->cmd_count == 1)
-		ft_putstr_fd("exit\n", STDERR_FILENO);
 	if (simple->argv && simple->argv[1])
 	{
-		ret = ft_atoi(simple->argv[1]);
+		num = ft_atoi_ret(simple->argv[1], &ret);
+		if (ret == -1)
+		{
+			ft_putstr_fd("minishell: exit: ", STDERR_FILENO);
+			ft_putstr_fd(simple->argv[1], STDERR_FILENO);
+			ft_putstr_fd(": numeric argument required\n", STDERR_FILENO);
+			exit (255);
+		}
+		else
+		{
+			ft_putstr_fd("exit\n", STDERR_FILENO);
+		}
 		clear_cmd(cmd);
-		exit (ret);
+		exit (num);
 	}
 	clear_cmd(cmd);
 	exit (g_errno);
